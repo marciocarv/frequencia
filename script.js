9d88aa7f4e22a45de04b38b6b36723ddd296c603
marciocarv <marciotibapa@gmail.com>
2021-12-14 07:46:09 -0300

    correcoes

--- script.js
+++ script.js
@@ -1,19 +1,379 @@
 var servidor = [
     {
+        'nome':'ALESSANDRO MARTINS BRUNO',
+        'cargo':'PROFESSOR III',
+        'funcao':'AUX. LABIN',
+        'vinculo': 'EFETIVO',
+        'matricula': '259481',
+        'contato': '63 8405-6211'
+    },
+	{
+        'nome':'ALEX ALVES DA SILVA',
+        'cargo':'PROFESSOR II',
+        'funcao':'DIRETOR',
+        'vinculo': 'EFETIVO',
+        'matricula': '378391',
+        'contato': '63 8409-6198'
+    },
+	{
+        'nome':'ARLETE CARVALHO SILVA',
+        'cargo':'AAE',
+        'funcao':'LIMPEZA',
+        'vinculo': 'CONTRATO',
+        'matricula': '413042784',
+        'contato': '63 9933-8846'
+    },
+	{
+        'nome':'ATLAS ARAUJO PONCE',
+        'cargo':'PROFESSOR II',
+        'funcao':'SUP. PEDAGOGICO',
+        'vinculo': 'EFETIVO',
+        'matricula': '195462',
+        'contato': '63 8437-3828'
+    },
+	{
+        'nome':'CARLIANIA PEREIRA CESAR SANTANA DA SILVA',
+        'cargo':'AAE',
+        'funcao':'MERENDA',
+        'vinculo': 'EFETIVO',
+        'matricula': '413018208',
+        'contato': '63 8429-5115'
+    },
+	{
+        'nome':'CLEIDE MARTINS OLIVEIRA',
+        'cargo':'AAE',
+        'funcao':'CUIDADORA',
+        'vinculo': 'EFETIVO',
+        'matricula': '413010893',
+        'contato': '63 63 9228-4900'
+    },
+	{
+        'nome':'CRISTIANE GOMES DE FREITAS AGUIAR',
+        'cargo':'AAE',
+        'funcao':'LIMPEZA',
+        'vinculo': 'EFETIVO',
+        'matricula': '413007826',
+        'contato': '63 8512-4685'
+    },
+	{
+        'nome':'DAMIÃO CONSTANTINO DOS SANTOS',
+        'cargo':'AAE',
+        'funcao':'COORD. DE APOIO',
+        'vinculo': 'EFETIVO',
+        'matricula': '413007603',
+        'contato': '-'
+    },
+	{
+        'nome':'DAYANE ALMEIDA VIEIRA',
+        'cargo':'TAE',
+        'funcao':'AUX. SECRETARIA',
+        'vinculo': 'EFETIVO',
+        'matricula': '413018488',
+        'contato': '63 9234-7734'
+    },
+	{
+        'nome':'DENISE DE OLIVEIRA',
+        'cargo':'PROFESSOR II',
+        'funcao':'ORIENTADORA',
+        'vinculo': 'EFETIVO',
+        'matricula': '51481',
+        'contato': '63 8515-6168'
+    },
+	{
+        'nome':'DOMINGOS FRANCISCO RODRIGUES',
+        'cargo':'AAE',
+        'funcao':'VIGIA DIURNO',
+        'vinculo': 'EFETIVO',
+        'matricula': '',
+        'contato': '63 98158-5303'
+    },
+	{
+        'nome':'DIVINO ROBERTO MESQUITA',
+        'cargo':'AAE',
+        'funcao':'VIGIA NOTURNO',
+        'vinculo': 'EFETIVO',
+        'matricula': '',
+        'contato': ''
+    },
+	{
+        'nome':'ENIVALDA LIMA DE MORAIS',
+        'cargo':'AAE',
+        'funcao':'LIMPEZA',
+        'vinculo': 'CONTRATO',
+        'matricula': '-',
+        'contato': '-'
+    },
+	{
+        'nome':'ELISSAMA DE OLIVEIRA MEDEIROS',
+        'cargo':'TAE',
+        'funcao':'AUXILIAR DE SECRETARIA',
+        'vinculo': 'CONTRATO',
+        'matricula': '--',
+        'contato': '-'
+    },
+	{
+        'nome':'JOENILSON OLIVEIRA SERRA DA SILVA',
+        'cargo':'PROFESSOR II',
+        'funcao':'SUP. PEDAGOGICO',
+        'vinculo': 'EFETIVO',
+        'matricula': '413018623',
+        'contato': '63 8108-0679'
+    },
+	{
+        'nome':'JOSE UILSON VALADARES DE SOUZA',
+        'cargo':'AAE',
+        'funcao':'VIGIA NOTURNO',
+        'vinculo': 'EFETIVO',
+        'matricula': '301371',
+        'contato': '63 8500-1282'
+    },
+	{
+        'nome':'LILIAN DE MOURA MARTINS TAVARES',
+        'cargo':'PROFESSOR II',
+        'funcao':'SUP. PEDAGOGICO',
+        'vinculo': 'EFETIVO',
+        'matricula': '312341',
+        'contato': '63 8441-7958'
+    },
+	{
+        'nome':'MADALENA LIMA DE MORAES',
+        'cargo':'AAE',
+        'funcao':'MERENDA',
+        'vinculo': 'EFETIVO',
+        'matricula': '413017309',
+        'contato': '63 9264-4444'
+    },
+	{
         'nome':'MARCIO CARVALHO DOS SANTOS',
         'cargo':'TAE',
         'funcao':'SECRETARIO',
         'vinculo': 'EFETIVO',
-        'matricula': '4154445',
-        'contato': '6398484-8484'
+        'matricula': '382261',
+        'contato': '63 8404-2389'
     },
-    {
-        'nome':'MARIA CLEIDE DA SILVA',
+	{
+        'nome':'MARIA CLEIDE RIBEIRO PINTO',
+        'cargo':'AAE',
+        'funcao':'AUX. SECRETARIA',
+        'vinculo': 'EFETIVO',
+        'matricula': '413012326',
+        'contato': '63 8473-8063'
+    },
+	{
+        'nome':'MARIA DO LIVRAMENTO PINTO DIAS',
+        'cargo':'AAE',
+        'funcao':'LIMPEZA',
+        'vinculo': 'CONTRATO',
+        'matricula': '413043363',
+        'contato': '63 8143-2064'
+    },
+	{
+        'nome':'MARIA DE JESUS XAVIER SILVA',
+        'cargo':'413040464',
+        'funcao':'AAE',
+        'vinculo': 'CONTRATO',
+        'matricula': '413040464',
+        'contato': '63 8496-1588'
+    },
+	{
+        'nome':'MARILENE DE ARAUJO SOARES',
+        'cargo':'AAE',
+        'funcao':'VIGIA DIURNO',
+        'vinculo': 'EFETIVO',
+        'matricula': '413012323',
+        'contato': '63 9213-6287'
+    },
+	{
+        'nome':'OSEANE ALMEIDA GALVAO',
+        'cargo':'AAE',
+        'funcao':'CAF',
+        'vinculo': 'EFETIVO',
+        'matricula': '413019759',
+        'contato': '63 8146-0803'
+    },
+	{
+        'nome':'RAIMUNDA HELENA CARVALHO DOS SANTOS',
+        'cargo':'AAE',
+        'funcao':'MERENDA',
+        'vinculo': 'EFETIVO',
+        'matricula': '41301769',
+        'contato': '63 8424-4151'
+    },
+	{
+        'nome':'REJANE SILVA SOUSA',
+        'cargo':'AAE',
+        'funcao':'MERENDA',
+        'vinculo': 'EFETIVO',
+        'matricula': ' ',
+        'contato': ' '
+    },
+	{
+        'nome':'SEBASTIÃO MARQUES DA SILVA',
+        'cargo':'AAE',
+        'funcao':'VIGIA DIURNO',
+        'vinculo': 'CONTRATO',
+        'matricula': '413042839',
+        'contato': '-'
+    },
+	{
+        'nome':'SIRLENE RODRIGUES OLIVEIRA',
+        'cargo':'AAE',
+        'funcao':'MERENDA',
+        'vinculo': 'EFETIVO',
+        'matricula': '413019808',
+        'contato': '63 8148-3277'
+    },
+	{
+        'nome':'VALDEMIR PEREIRA ALVES',
+        'cargo':'AAE',
+        'funcao':'VIGIA DIURNO',
+        'vinculo': 'CONTRATO',
+        'matricula': '413039914',
+        'contato': '63 9919-2480'
+    },
+	{
+        'nome':'VANILDO PEREIRA PINTO',
         'cargo':'AAE',
         'funcao':'VIGIA NOTURNO',
         'vinculo': 'EFETIVO',
-        'matricula': '45454545',
-        'contato': '6394454-8484'
+        'matricula': '139011',
+        'contato': '-'
+    },
+	{
+        'nome':'ALBENY TAVARES CORADO',
+        'cargo':'PROFESSOR II',
+        'funcao':'PROFESSOR DE HIST/GEO',
+        'vinculo': 'EFETIVO',
+        'matricula': '995031',
+        'contato': '63 8467-3331'
+    },
+	{
+		'nome':'ALEXANDER JOSÉ ALENCAR DE SOUSA',
+        'cargo':'PROFESSOR II',
+        'funcao':'PROFESSOR DE MATEMÁTICA',
+        'vinculo': 'CONTRATO',
+        'matricula': '369081',
+        'contato': '63 8477-1314'
+	},
+	{
+        'nome':'ANDECYWALLA MARINHO LIMA',
+        'cargo':'PROFESSOR II',
+        'funcao':'PROFESSOR DE PORTUGUÊS',
+        'vinculo': 'CONTRATO',
+        'matricula': '413040694',
+        'contato': '63 8103-0804'
+    },
+	{
+        'nome':'ELISABETE FERRARI CAZULA',
+        'cargo':'PROFESSOR II',
+        'funcao':'PROFESSOR DE CIENCIAS',
+        'vinculo': 'EFETIVO',
+        'matricula': '1082431',
+        'contato': '63 8114-0014'
+    },
+	{
+		'nome':'ELMORANNE OLIVEIRA DA SILVA',
+        'cargo':'PROFESSOR II',
+        'funcao':'PROFESSOR DE PORTUGUÊS',
+        'vinculo': 'CONTRATO',
+        'matricula': '--',
+        'contato': '63 98135-2160'
+	},
+	{
+        'nome':'FAUSTO GUILHARDI VIROTE',
+        'cargo':'PROFESSOR II',
+        'funcao':'PROFESSOR DE ED. FÍSICA',
+        'vinculo': 'CONTRATO',
+        'matricula': '413039357',
+        'contato': '63 8132-7922'
+    },
+	{
+        'nome':'FRANCISCO GILSON PEREIRA DA SILVA',
+        'cargo':'PROFESSOR II',
+        'funcao':'PROFESSOR DE MATEMÁTICA',
+        'vinculo': 'CONTRATO',
+        'matricula': '----',
+        'contato': '-'
+    },
+	{
+        'nome':'GECILENE DA SILVA OLIVEIRA',
+        'cargo':'PROFESSOR II',
+        'funcao':'PROFESSOR DE HISTÓRIA',
+        'vinculo': 'EFETIVO',
+        'matricula': '334241',
+        'contato': '63 8434-5675'
+    },
+	{
+        'nome':'HIULA MENDES RODRIGUES',
+        'cargo':'PROFESSOR I',
+        'funcao':'PROFESSOR AUXILIAR',
+        'vinculo': 'EFETIVO',
+        'matricula': '413017855',
+        'contato': '63 9911-5452'
+    },
+	{
+        'nome':'JOSELIO CHAGAS LIMA',
+        'cargo':'PROFESSOR II',
+        'funcao':'PROFESSOR SÉRIES FINAIS',
+        'vinculo': 'EFETIVO',
+        'matricula': '413017831',
+        'contato': '63 9220-9877'
+    },
+	{
+        'nome':'JOSE PEREIRA DOS SANTOS FILHO',
+        'cargo':'PROFESSOR III',
+        'funcao':'PROFESSOR DE PORTUGUÊS',
+        'vinculo': 'EFETIVO',
+        'matricula': '413017829',
+        'contato': '63 9253-9472'
+    },
+	{
+        'nome':'LUIZ ALBERTO BIANCHINI',
+        'cargo':'PROFESSOR II',
+        'funcao':'PROFESSOR DE ED. FÍSICA',
+        'vinculo': 'EFETIVO',
+        'matricula': '312081',
+        'contato': '63 8121-7401'
+    },
+	{
+        'nome':'MANOEL BONFIM VIEIRA DOS SANTOS NETO',
+        'cargo':'PROFESSOR II',
+        'funcao':'PROFESSOR DE MATEMÁTICA',
+        'vinculo': 'EFETIVO',
+        'matricula': '255591',
+        'contato': '63 8402-9029'
+    },
+	{
+        'nome':'MARIA JOSE DA CONCEICAO',
+        'cargo':'PROFESSOR III',
+        'funcao':'PROFESSOR DE GEOGRAFIA',
+        'vinculo': 'EFETIVO',
+        'matricula': '261021',
+        'contato': '63 9992-0105'
+    },
+	{
+        'nome':'SEBASTIAO DO BONFIM CARLOS RAMALHO',
+        'cargo':'PROFESSOR II',
+        'funcao':'PROFESSOR SERIES INICIAIS',
+        'vinculo': 'EFETIVO',
+        'matricula': '313721',
+        'contato': '63 9218-8660'
+    },
+	{
+        'nome':'SUZANA MANOEL DA SILVA',
+        'cargo':'PROFESSOR II',
+        'funcao':'PROFESSORA DE CIÊNCIAS',
+        'vinculo': 'CONTRATO',
+        'matricula': '413039693',
+        'contato': '63 8140-6698'
+    },
+	{
+        'nome':'WILDSON DE JESUS DOS SANTOS',
+        'cargo':'PROFESSOR I',
+        'funcao':'PROFESSOR SÉRIES INICIAIS',
+        'vinculo': 'EFETIVO',
+        'matricula': '413017968',
+        'contato': '63 8440-3822'
     },
 ];
 
